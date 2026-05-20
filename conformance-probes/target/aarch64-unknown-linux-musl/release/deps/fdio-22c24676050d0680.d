e9a7f5441579a8590e0a121685c4488141f51816
Timothy J Fontaine <tjfontaine@atxconsulting.com>
2026-05-20 14:46:57 -0700

    gitignore conformance-probes/target; untrack 141 committed build artifacts

--- conformance-probes/target/aarch64-unknown-linux-musl/release/deps/fdio-22c24676050d0680.d
+++
@@ -1,5 +0,0 @@
-/p/target/aarch64-unknown-linux-musl/release/deps/fdio-22c24676050d0680.d: src/bin/fdio.rs
-
-/p/target/aarch64-unknown-linux-musl/release/deps/fdio-22c24676050d0680: src/bin/fdio.rs
-
-src/bin/fdio.rs:
--- conformance-probes/target/aarch64-unknown-linux-musl/release/fdio.d
+++
@@ -1,1 +0,0 @@
-/p/target/aarch64-unknown-linux-musl/release/fdio: /p/src/bin/fdio.rs
